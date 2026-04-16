8e9cb3a22315931a97c53c4dd60bfa1d0b5db7c1
Fiona C <106705082+crookf@users.noreply.github.com>
2026-04-16 22:59:54 +0000

    Connect app to local fhir

---
+++ examples/fhir-usage/Program.cs
@@ -0,0 +1,22 @@
+﻿using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+
+namespace MyFirstFhirClient
+{
+  internal class Program
+  {
+    static async System.Threading.Tasks.Task Main(string[] args)
+    {
+      var client = new FhirClient("http://localhost:8080/fhir/r4");
+       //client.Create(patient);
+       Bundle? searchResult = await client.SearchAsync<Medication>();
+
+       foreach (var result in (searchResult?.Entry ?? Enumerable.Empty<Bundle.EntryComponent>()))
+       {
+         var med = result.Resource as Medication;
+         Console.WriteLine($"Received medication with {med?.Code?.Text}");
+       }
+
+    }
+  }
+}
